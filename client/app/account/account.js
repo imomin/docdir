cee53988922f93c0a57bc824404b7884076d02fd
imomin <imomin@gmail.com>
2015-04-14 23:56:29 -0500

    relocated account/doctor folder.

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -21,65 +21,65 @@ angular.module('sugarlandDoctorsApp')
       })
       .state('doctor', {
         url: '/doctor',
-        templateUrl: 'app/account/signup/doctor/index.html',
+        templateUrl: 'app/account/doctor/index.html',
         controller:'doctorSignupCtrl'
       })
       .state('doctor.emailVerification', {
         url: '/thankyou',
-        templateUrl: 'app/account/signup/doctor/emailVerification.html'
+        templateUrl: 'app/account/doctor/emailVerification.html'
       })
       .state('doctor.signup', {
         url: '/signup',
-        templateUrl: 'app/account/signup/doctor/signup.html'
+        templateUrl: 'app/account/doctor/signup.html'
       })
       .state('doctor.login', {
         url: '/login',
-        templateUrl: 'app/account/signup/doctor/login.html',
+        templateUrl: 'app/account/doctor/login.html',
         controller:'doctorLoginCtrl'
       })
       .state('doctor.resetPassword', {
         url: '/reset',
-        templateUrl: 'app/account/signup/doctor/passwordReset.html',
+        templateUrl: 'app/account/doctor/passwordReset.html',
         controller:'doctorLoginCtrl'
       })
       .state('doctor.profile', {
         url: '/profile',
-        templateUrl: 'app/account/signup/doctor/profile.html',
+        templateUrl: 'app/account/doctor/profile.html',
         authenticate: true
       })
       .state('doctor.profile.bio', {
         url: '/bio',
-        templateUrl: 'app/account/signup/doctor/bio.html',
+        templateUrl: 'app/account/doctor/bio.html',
         authenticate: true
       })
       .state('doctor.profile.education', {
         url: '/education',
-        templateUrl: 'app/account/signup/doctor/education.html',
+        templateUrl: 'app/account/doctor/education.html',
         authenticate: true
       })
       .state('doctor.profile.featuers', {
         url: '/featuers',
-        templateUrl: 'app/account/signup/doctor/features.html',
+        templateUrl: 'app/account/doctor/features.html',
         authenticate: true
       })
       .state('doctor.profile.pictures', {
         url: '/pictures',
-        templateUrl: 'app/account/signup/doctor/pictures.html',
+        templateUrl: 'app/account/doctor/pictures.html',
         authenticate: true
       })
       .state('doctor.profile.address', {
         url: '/address',
-        templateUrl: 'app/account/signup/doctor/address.html',
+        templateUrl: 'app/account/doctor/address.html',
         authenticate: true
       })
       .state('doctor.profile.subscribe', {
         url: '/subscribe',
-        templateUrl: 'app/account/signup/doctor/subscribe.html',
+        templateUrl: 'app/account/doctor/subscribe.html',
         authenticate: true
       })      
       .state('doctor.profile.finish', {
         url: '/finish',
-        templateUrl: 'app/account/signup/doctor/finish.html',
+        templateUrl: 'app/account/doctor/finish.html',
         authenticate: true
       });
   });
