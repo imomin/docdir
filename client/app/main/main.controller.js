63be98583c9cfc4b47bb675a15fa45a35cc84fa0
imomin <imomin@gmail.com>
2015-05-16 16:30:55 -0500

    Homepage work

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,42 @@
 'use strict';
 
 angular.module('sugarlandDoctorsApp')
+  .directive('afloat', function(){
+    // Runs during compile
+    return {
+      // name: '',
+      // priority: 1,
+      // terminal: true,
+      // scope: {}, // {} = isolate, true = child, false/undefined = no change
+      // controller: function($scope, $element, $attrs, $transclude) {},
+      // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+       restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+      // template: '',
+      // templateUrl: '',
+      // replace: true,
+      // transclude: true,
+      // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+      link: function($scope, iElm, iAttrs, controller) {
+          var $window = angular.element(window);
+          function showNavbar(e,v){
+            debugger;
+            //iElm.offset().top = $("header").height() - iElm.height();
+            if(1==1){
+              iElm.removeClass('fixed-header').css({
+                                                  'display': 'block'
+                                              }).fadeIn(300);
+            }
+            else {
+              iElm.addClass('fixed-header').css({
+                                    'display': 'none'
+                                }).fadeIn(300);
+            }
+          };
+
+          $window.on('scroll',  showNavbar);
+      }
+    };
+  })
   .controller('MainCtrl', function ($scope, $http, socket,page) {
     $scope.awesomeThings = [];
     page.setTitle("home page");
